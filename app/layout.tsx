ae1e771fe18ce6815a12227ac17253fb93501d02
iiebi <bruxell456@gmail.com>
2024-08-28 22:01:55 +0200

    fixing metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,21 @@ export const metadata: Metadata = {
   title: "eField League",
   description:
     "Where every play matters, and every player shines. Welcome to eField.",
+  openGraph: {
+    title: "eField League",
+    description:
+      "Where every play matters, and every player shines. Welcome to eField.",
+    url: "https://nextjs.org/learn/dashboard-app/adding-metadata",
+    type: "website",
+    images: [
+      {
+        url: "/NoBG_White.png",
+        width: 1200,
+        height: 630,
+        alt: "eField League Open Graph Image",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
