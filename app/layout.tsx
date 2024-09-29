800aebf808f4ddf576899fbe9907fa3d482f947e
iiebi <bruxell456@gmail.com>
2024-09-29 18:16:43 +0200

    add mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer />
-        {/* <Loading /> */}
+         <Loading /> 
       </body>
     </html>
   );
